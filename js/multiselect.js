407a1345db19d6af192785fa099ffc2d93ffff69
Adrian Crisan <crycul2oo4@yahoo.com>
2013-04-03 15:31:43 +0300

    get closer to version 1

--- js/multiselect.js
+++ js/multiselect.js
@@ -1,187 +1,263 @@
-;(function( $ ){
-	$.fn.multiselect = function( config ) {
-		var $this = this;
+/*
+ * Multiselect
+ * https://github.com/crlcu/multiselect
+ *
+ * Copyright (c) 2013 Adrian Crisan
+ * Licensed under the MIT license.
+ */
+
+;(function( $ ) {
+	var Multiselect = (function( $ ){
+		"use strict";
 		
-		var defaults = {
-			init: undefined,
-			beforeMoveOneToRight: function($left, $right, option){ return true; },
+		/**
+		Multiselect object constructor
+		
+		@class Multiselect
+		@constructor
+		**/
+		function Multiselect( $select, settings ) {			
+			if ( typeof $ != 'function' ) {
+				throw "$ is undefined or not a function";
+			}
+			
+			this.left = $select;
+			this.right = $( settings.right );
+			this.actions = {					
+				leftAll: 		$( settings.leftAll ),
+				rightAll: 		$( settings.rightAll ),
+				leftSelected:	$( settings.leftSelected ),
+				rightSelected:	$( settings.rightSelected )
+			};
+			
+			delete settings.leftAll;
+			delete settings.leftSelected;
+			delete settings.right;
+			delete settings.rightAll;
+			delete settings.rightSelected;
+			
+			this.callbacks = settings;
+			
+			this.init();
+		}
+		
+		Multiselect.prototype = {
+			init: function() {
+				var self = this;
+				
+				if ( typeof self.callbacks.startUp == 'function' ) {
+					self.callbacks.startUp( self.left, self.right );
+				}
+				
+				if ( typeof self.callbacks.sort == 'function' ) {
+					self.left.find('option').sort(self.callbacks.sort).appendTo(self.left);
+					self.right.find('option').sort(self.callbacks.sort).appendTo(self.right);
+				}
+				
+				self.events( self.actions );
+			},
+			
+			events: function( actions ) {
+				var self = this;
+				
+				self.left.on('dblclick', 'option', function(e) {
+					e.preventDefault();
+					self.moveOneToRight(this);
+				});
+				
+				self.right.on('dblclick', 'option', function(e) {
+					e.preventDefault();
+					self.moveOneToLeft(this);
+				});
+				
+				// dblclick support for IE
+				if ( navigator.userAgent.match(/MSIE/i) ) {
+					self.left.dblclick(function(e){
+						actions.rightSelected.trigger('click');
+					});
+					
+					self.right.dblclick(function(e){
+						actions.leftSelected.trigger('click');
+					});
+				}
+				
+				actions.rightSelected.on('click', function(e) {
+					e.preventDefault();
+					var option = self.left.find('option:selected');
+					
+					if ( option )
+						self.moveOneToRight(option);
+				});
+				
+				actions.leftSelected.on('click', function(e) {
+					e.preventDefault();
+					var option = self.right.find('option:selected');
+					
+					if ( option )
+						self.moveOneToLeft(option);
+				});
+				
+				actions.rightAll.on('click', function(e){
+					e.preventDefault();
+					var options = self.left.find('option');
+					
+					if ( options )
+						self.moveAllToRight(options);
+				});
+				
+				actions.leftAll.on('click', function(e){
+					e.preventDefault();
+					
+					var options = self.right.find('option');
+					
+					if ( options )
+						self.moveAllToLeft(options);
+				});
+			},
+			
+			moveOneToRight: function( option, silent ) {
+				var self = this;
+				
+				if ( typeof self.callbacks.beforeMoveOneToRight == 'function' && !silent ) {
+					if ( !self.callbacks.beforeMoveOneToRight( self.left, self.right, option ) )
+						return false;
+				}
+				
+				self.right.append(option);
+				
+				if ( typeof self.callbacks.sort == 'function' && !silent ) {
+					self.right.find('option').sort(self.callbacks.sort).appendTo(self.right);		
+				}
+				
+				if ( typeof self.callbacks.afterMoveOneToRight == 'function' && !silent ){
+					self.callbacks.afterMoveOneToRight( self.left, self.right, option );
+				}
+				
+				return self;
+			},
+			
+			moveOneToLeft: function( option, silent ) {
+				var self = this;
+				
+				if ( typeof self.callbacks.beforeMoveOneToLeft == 'function' && !silent ) {
+					if ( !self.callbacks.beforeMoveOneToLeft( self.left, self.right, option ) )
+						return false;
+				}
+					
+				self.left.append(option);
+				
+				if ( typeof self.callbacks.sort == 'function' && !silent ) {
+					self.left.find('option').sort(self.callbacks.sort).appendTo(self.left);		
+				}
+				
+				if ( typeof self.callbacks.afterMoveOneToLeft == 'function' && !silent ) {
+					self.callbacks.afterMoveOneToLeft( self.left, self.right, option );	
+				}
+				
+				return self;
+			},
+			
+			moveAllToRight: function( options ) {
+				var self = this;
+				
+				if ( typeof self.callbacks.beforeMoveAllToRight == 'function' ) {
+					if ( !self.callbacks.beforeMoveAllToRight( self.left, self.right, options ) )
+						return false;
+				}
+				
+				for ( var i = 0; options[i]; i++ ) {
+					self.moveOneToRight(options[i], true);
+				}
+				
+				if ( typeof self.callbacks.sort == 'function' ) {
+					self.right.find('option').sort(self.callbacks.sort).appendTo(self.right);		
+				}
+				
+				if ( typeof self.callbacks.afterMoveAllToRight == 'function' ){
+					self.callbacks.afterMoveAllToRight( self.left, self.right, options );	
+				}
+				
+				return self;
+			},
+			
+			moveAllToLeft: function( options ) {
+				var self = this;
+				
+				if ( typeof self.callbacks.beforeMoveAllToLeft == 'function' ) {
+					if ( !self.callbacks.beforeMoveAllToLeft( self.left, self.right, options ) )
+						return false;
+				}
+				
+				for ( var i = 0; options[i]; i++ ) {
+					self.moveOneToLeft(options[i], true);
+				}
+				
+				if ( typeof self.callbacks.sort == 'function' ){
+					self.left.find('option').sort(self.callbacks.sort).appendTo(self.left);		
+				}
+				
+				if ( typeof self.callbacks.afterMoveAllToLeft == 'function' ){
+					self.callbacks.afterMoveAllToLeft( self.left, self.right, options );	
+				}
+				
+				return self;
+			}
+		}
+		
+		return Multiselect;
+	})( jQuery );
+	
+	$.multiselect = {
+		defaults : {
+			/*
+			 * will be executed once
+			 * @method startUp
+			**/
+			startUp: function( $left, $right ) {},
+			/*
+			 *  will be executed each time before moving one option to right
+			 *  IMPORTANT : this method must return boolean value
+			 *      true    : continue to moveOneToRight method
+			 *      false   : stop
+			 * 
+			 *  @method beforeMoveOneToRight
+			 *  @attribute $left jQuery object
+			 *  @attribute $right jQuery object
+			 *  @attribute option HTML object (the option which was selected to be moved)
+			 *  
+			 *  @default true
+			 *  @return {boolean}
+			**/
+			beforeMoveOneToRight: function($left, $right, option) { return true; },
 			afterMoveOneToRight: function($left, $right, option){},
 			beforeMoveAllToRight: function($left, $right, options){ return true; },
 			afterMoveAllToRight: function($left, $right, options){},
 			beforeMoveOneToLeft: function($left, $right, option){ return true; },
 			afterMoveOneToLeft: function($left, $right, option){},
 			beforeMoveAllToLeft: function($left, $right, options){ return true; },
-			afterMoveAllToLeft: function($left, $right, options){}
-		};
-		
-		config = $.extend({}, defaults, config);
-		
-		var mcs = {
-			$msc_left: undefined,
-			$msc_right: undefined,
-			$rightAll: undefined,
-			$rightSelected: undefined,
-			$leftAll: undefined,
-			$leftSelected: undefined,
-			
-			init: function( $msc_left ){				
-				var data = $msc_left.data();
-				
-				mcs.$msc_left = $msc_left;
-				mcs.$msc_right = $( data.right );
-				
-				mcs.$rightAll = $( data.rightAll );
-				mcs.$rightSelected = $( data.rightSelected );
-				
-				mcs.$leftAll = $( data.leftAll );
-				mcs.$leftSelected = $( data.leftSelected );
-				
-				if ( typeof config.init == 'function' ){
-					config.init( mcs.$msc_left, mcs.$msc_right );	
+			afterMoveAllToLeft: function($left, $right, options){},
+			sort: function(a, b) {
+				if (a.innerHTML == 'NA') {
+					return 1;   
+				} else if (b.innerHTML == 'NA') {
+					return -1;   
 				}
 				
-				if ( typeof config.sort == 'function' ){
-					mcs.$msc_left.find('option').sort(config.sort).appendTo(mcs.$msc_left);
-					mcs.$msc_right.find('option').sort(config.sort).appendTo(mcs.$msc_right);
-				}
-				
-				if ( $.browser.msie ){
-					mcs.$msc_left.dblclick(function(e){
-						mcs.$rightSelected.trigger('click');
-					});
-					
-					mcs.$msc_right.dblclick(function(e){
-						mcs.$leftSelected.trigger('click');
-					});
-				}
-				
-				var $form = $msc_left.closest('form');
-				$form.off('submit').on('submit', function(){
-					$('select[multiple]', $form).find('option').attr('selected', true);
-				});
-				
-				mcs.$msc_left.on('dblclick', 'option', function(e){
-					e.preventDefault();
-					mcs.moveOneToRight(this);
-				});
-				
-				mcs.$msc_right.on('dblclick', 'option', function(e){
-					e.preventDefault();
-					mcs.moveOneToLeft(this);
-				});
-				
-				mcs.$rightAll.on('click', function(e){
-					e.preventDefault();
-					
-					var options = mcs.$msc_left.find('option');
-					
-					if ( options )
-						mcs.moveAllToRight(options);
-				});
-				
-				mcs.$rightSelected.on('click', function(e){
-					e.preventDefault();
-					var option = mcs.$msc_left.find('option:selected');
-					
-					if ( option )
-						mcs.moveOneToRight(option);
-				});
-				
-				mcs.$leftAll.on('click', function(e){
-					e.preventDefault();
-					
-					var options = mcs.$msc_right.find('option');
-					
-					if ( options )
-						mcs.moveAllToLeft(options);
-				});
-				
-				mcs.$leftSelected.on('click', function(e){
-					e.preventDefault();
-					var option = mcs.$msc_right.find('option:selected');
-					
-					if ( option )
-						mcs.moveOneToLeft(option);
-				});
-			},
-			
-			moveOneToRight: function( option, silent ){
-				if ( typeof config.beforeMoveOneToRight == 'function' && !silent ){
-					if ( !config.beforeMoveOneToRight( mcs.$msc_left, mcs.$msc_right, option ) )
-						return false;
-				}
-				
-				mcs.$msc_right.append(option);
-				
-				if ( typeof config.sort == 'function' && !silent ){
-					mcs.$msc_right.find('option').sort(config.sort).appendTo(mcs.$msc_right);		
-				}
-				
-				if ( typeof config.afterMoveOneToRight == 'function' && !silent ){
-					config.afterMoveOneToRight( mcs.$msc_left, mcs.$msc_right, option );
-				}
-			},
-			
-			moveAllToRight: function( options ){
-				if ( typeof config.beforeMoveAllToRight == 'function' ){
-					if ( !config.beforeMoveAllToRight( mcs.$msc_left, mcs.$msc_right, options ) )
-						return false;
-				}
-				
-				for ( var i = 0; options[i]; i++ ){
-					mcs.moveOneToRight(options[i], true);
-				}
-				
-				if ( typeof config.sort == 'function' ){
-					mcs.$msc_right.find('option').sort(config.sort).appendTo(mcs.$msc_right);		
-				}
-				
-				if ( typeof config.afterMoveAllToRight == 'function' ){
-					config.afterMoveAllToRight( mcs.$msc_left, mcs.$msc_right, options );	
-				}
-			},
-			
-			moveOneToLeft: function( option, silent ){
-				if ( typeof config.beforeMoveOneToLeft == 'function' && !silent ){
-					if ( !config.beforeMoveOneToLeft( mcs.$msc_left, mcs.$msc_right, option ) )
-						return false;
-				}
-					
-				mcs.$msc_left.append(option);
-				
-				if ( typeof config.sort == 'function' && !silent ){
-					mcs.$msc_left.find('option').sort(config.sort).appendTo(mcs.$msc_left);		
-				}
-				
-				if ( typeof config.afterMoveOneToLeft == 'function' && !silent ){
-					config.afterMoveOneToLeft( mcs.$msc_left, mcs.$msc_right, option );	
-				}
-			},
-			
-			moveAllToLeft: function( options ){
-				if ( typeof config.beforeMoveAllToLeft == 'function' ){
-					if ( !config.beforeMoveAllToLeft( mcs.$msc_left, mcs.$msc_right, options ) )
-						return false;
-				}
-				
-				for ( var i = 0; options[i]; i++ ){
-					mcs.moveOneToLeft(options[i], true);
-				}
-				
-				if ( typeof config.sort == 'function' ){
-					mcs.$msc_left.find('option').sort(config.sort).appendTo(mcs.$msc_left);		
-				}
-				
-				if ( typeof config.afterMoveAllToLeft == 'function' ){
-					config.afterMoveAllToLeft( mcs.$msc_left, mcs.$msc_right, options );	
-				}
+				return (a.innerHTML > b.innerHTML) ? 1 : -1;
 			}
-		};
-		
-		$this.each(function() {			
-			mcs.init( $(this) );
-		});
-		
-		return $this;
-	};
+		},
+		setup: {}
+	}
+    
+    $.fn.multiselect = function( options ) {        
+        return this.each(function() {	
+			var $this = $(this),
+				data = $this.data();
+			
+			var settings = $.extend({}, $.multiselect.defaults, $.multiselect.setup, data, options);
+			
+            return new Multiselect($(this), settings);
+        });  
+    };
+    
 })( jQuery );
