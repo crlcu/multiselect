1b83324a697ee16c438d815d6fe41bf9ae082fce
Adrian Crisan <crycul2oo4@yahoo.com>
2015-06-01 09:33:47 +0300

    Keep render sort for right side

--- js/multiselect.js
+++ js/multiselect.js
@@ -1,4 +1,6 @@
-/*!
+/*
+ * @license
+ *
  * Multiselect v2.1.1
  * http://crlcu.github.io/multiselect/
  *
@@ -21,13 +23,13 @@ if (typeof jQuery === 'undefined') {
 })(jQuery);
 
 ;(function (factory) {
-   	if (typeof define === 'function' && define.amd) {
-     	// AMD. Register as an anonymous module depending on jQuery.
-     	define(['jquery'], factory);
-   	} else {
-    	// No AMD. Register plugin with global jQuery object.
-    	factory(jQuery);
-   	}
+	if (typeof define === 'function' && define.amd) {
+		// AMD. Register as an anonymous module depending on jQuery.
+		define(['jquery'], factory);
+	} else {
+		// No AMD. Register plugin with global jQuery object.
+		factory(jQuery);
+	}
 }(function ($) {
 	'use strict';
 
@@ -85,6 +87,10 @@ if (typeof jQuery === 'undefined') {
 					};
 
 					self.left.find('option').each(function(index, option) {
+						$(option).data('position', index);
+					});
+
+					self.right.find('option').each(function(index, option) {
 						$(option).data('position', index);
 					});
 				}
@@ -380,15 +386,15 @@ if (typeof jQuery === 'undefined') {
 			}
 		}
 	};
-    
-    $.fn.multiselect = function( options ) {
-        return this.each(function() {
+
+	$.fn.multiselect = function( options ) {
+		return this.each(function() {
 			var $this = $(this),
 				data = $this.data();
 			
 			var settings = $.extend({}, $.multiselect.defaults, data, options);
 			
-            return new Multiselect($this, settings);
-        });  
-    };
+			return new Multiselect($this, settings);
+		});
+	};
 }));
