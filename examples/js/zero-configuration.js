6abd246ea3fa2900961c18d3d8a0978197a6076c
Daniel Poggenpohl <daniel.poggenpohl@fernuni-hagen.de>
2017-06-19 14:16:26 +0200

    Added zero configuration example with separate js configuration file and removed base URL. Additional jsdoc in config file

---
+++ examples/js/zero-configuration.js
@@ -0,0 +1,42 @@
+/**
+ * Created by poggenpohlda on 19.06.2017.
+ * Separate JS file for example for better readability.
+ */
+
+$(document).ready(function() {
+    "use strict";
+    // make code pretty
+    window.prettyPrint && prettyPrint();
+
+    console.log(navigator.userAgent);
+
+    // hack for iPhone 7.0.3 multiselects bug
+    if(navigator.userAgent.match(/iPhone/i)) {
+        $('select[multiple]').each(function(){
+            /** @type {jQuery} select */
+            var select = $(this).on({
+                "focusout": function(){
+                    /** @type {string[]|number[]} values */
+                    var values = select.val() || [];
+                    setTimeout(function(){
+                        select.val(values.length ? values : ['']).change();
+                    }, 1000);
+                }
+            });
+            /** @type {string} firstOption */
+            var firstOption = '<option value="" disabled="disabled"';
+            firstOption += (select.val() || []).length > 0 ? '' : ' selected="selected"';
+            firstOption += '>Select ' + (select.attr('title') || 'Options') + '';
+            firstOption += '</option>';
+            select.prepend(firstOption);
+        });
+    }
+
+    $('#multiselect').multiselect({
+        keepRenderingSort: true,
+        sort: false
+        // sort: function() {
+        //
+        // }
+    });
+});
