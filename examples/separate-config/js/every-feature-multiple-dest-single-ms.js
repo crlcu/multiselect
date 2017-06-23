2256409349d531045ebc72df1d90dff2265298cb
Daniel Poggenpohl <daniel.poggenpohl@fernuni-hagen.de>
2017-06-23 15:46:25 +0200

    importang thing goes up

--- examples/separate-config/js/every-feature-multiple-dest-single-ms.js
+++ examples/separate-config/js/every-feature-multiple-dest-single-ms.js
@@ -5,6 +5,21 @@ $(document).ready(function() {
     // make code pretty
     window.prettyPrint && prettyPrint();
 
+    $('#multi_d').multiselect({
+        right: '#multi_d_to, #multi_d_to_2',
+        rightSelected: '#multi_d_rightSelected, #multi_d_rightSelected_2',
+        leftSelected: '#multi_d_leftSelected, #multi_d_leftSelected_2',
+        rightAll: '#multi_d_rightAll, #multi_d_rightAll_2',
+        leftAll: '#multi_d_leftAll, #multi_d_leftAll_2',
+
+        search: {
+            left: '<input type="text" name="q" class="form-control" placeholder="Search..." />',
+            right: '<input type="text" name="q2" class="form-control" placeholder="Search..." />'
+        },
+        moveToRight: ownMoveToRight,
+        moveToLeft: ownMoveToLeft
+    });
+
     function ownMoveToRight(Multiselect, $options, event, silent, skipStack) {
         var button = $(event.currentTarget).attr('id');
 
@@ -73,21 +88,4 @@ $(document).ready(function() {
         }
     }
 
-    $('#multi_d').multiselect({
-        right: '#multi_d_to, #multi_d_to_2',
-        rightSelected: '#multi_d_rightSelected, #multi_d_rightSelected_2',
-        leftSelected: '#multi_d_leftSelected, #multi_d_leftSelected_2',
-        rightAll: '#multi_d_rightAll, #multi_d_rightAll_2',
-        leftAll: '#multi_d_leftAll, #multi_d_leftAll_2',
-
-        search: {
-            left: '<input type="text" name="q" class="form-control" placeholder="Search..." />',
-            right: '<input type="text" name="q2" class="form-control" placeholder="Search..." />'
-        },
-
-
-        moveToRight: ownMoveToRight,
-
-        moveToLeft: ownMoveToLeft
-    });
 });
