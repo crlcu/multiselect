c9ecf2f43c46a520da987717bc1bffc6f94fd3c1
Bernhard Fechner <Bernhard.Fechner@fernuni-hagen.de>
2017-06-26 15:47:47 +0200

    multi-multi example (extended by foreach...)

--- examples/separate-config/js/every-feature-multiple-dest-multiple-ms.js
+++ examples/separate-config/js/every-feature-multiple-dest-multiple-ms.js
@@ -7,47 +7,52 @@
 $(document).ready(function() {
     // make code pretty
     window.prettyPrint && prettyPrint();            // Why?
+    var msInstanceIdPrefixes = ["#multi_d_1","#multi_d_2"];
+    $(msInstanceIdPrefixes).each(function(i, prefix) {
+        $(prefix).multiselect({
+            right: prefix+'_to_1, ' + prefix + '_to_2',
+            rightSelected: prefix+'_rightSelected_1, ' + prefix + '_rightSelected_2',
+            leftSelected: prefix+'_leftSelected_1, ' + prefix + '_leftSelected_2',
+            rightAll: prefix+'_rightAll_1, ' + prefix + '_rightAll_2',
+            leftAll: prefix+'_leftAll_1, ' + prefix + '_leftAll_2',
+            moveUp: prefix+'_moveUp_1, ' + prefix + '_moveUp_2',
+            moveDown: prefix+'_moveDown_1, ' + prefix + '_moveDown_2',
 
-    $('#multiselect').multiselect({
-        right: '#multiselect_to, #multiselect_to_2',
-        rightSelected: '#multiselect1_rightSelected, #multiselect2_rightSelected',
-        leftSelected: '#multiselect1_leftSelected, #multiselect2_leftSelected',
-        rightAll: '#multiselect1_rightAll, #multiselect_rightAll',
-        leftAll: '#multiselect1_leftAll, #multiselect2_leftAll',
-
-        search: {
-            left: '<input type="text" name="q" class="form-control" placeholder="Search..." />',
-            right: '<input type="text" name="q2" class="form-control" placeholder="Search..." />'
-        },
-        moveToRight: ownMoveToRight,
-        moveToLeft: ownMoveToLeft
+            search: {
+                left: '<input type="text" name="q" class="form-control" placeholder="Search..." />',
+                right: '<input type="text" name="q2" class="form-control" placeholder="Search..." />'
+            },
+            moveToRight: ownMoveToRight,
+            moveToLeft: ownMoveToLeft
+        });
     });
 
     function ownMoveToRight(Multiselect, $options, event, silent, skipStack) {
         var button = $(event.currentTarget).attr('id');
 
-        if (button == 'multiselect1_rightSelected') {
+        var prefix = Multiselect.$left.attr("id");
+        if (button == prefix+'_rightSelected_1') {
             var $left_options = Multiselect.$left.find('> option:selected');
             Multiselect.$right.eq(0).append($left_options);
 
             if ( typeof Multiselect.callbacks.sort == 'function' && !silent ) {
                 Multiselect.$right.eq(0).find('> option').sort(Multiselect.callbacks.sort).appendTo(Multiselect.$right.eq(0));
             }
-        } else if (button == 'multiselect1_rightAll') {
+        } else if (button == prefix+'_rightAll_1') {
             var $left_options = Multiselect.$left.children(':visible');
             Multiselect.$right.eq(0).append($left_options);
 
             if ( typeof Multiselect.callbacks.sort == 'function' && !silent ) {
                 Multiselect.$right.eq(0).find('> option').sort(Multiselect.callbacks.sort).appendTo(Multiselect.$right.eq(0));
             }
-        } else if (button == 'multiselect2_rightSelected') {
+        } else if (button == prefix+'_rightSelected_2') {
             var $left_options = Multiselect.$left.find('> option:selected');
             Multiselect.$right.eq(1).append($left_options);
 
             if ( typeof Multiselect.callbacks.sort == 'function' && !silent ) {
                 Multiselect.$right.eq(1).find('> option').sort(Multiselect.callbacks.sort).appendTo(Multiselect.$right.eq(1));
             }
-        } else if (button == 'multiselect2_rightAll') {
+        } else if (button == prefix+'_rightAll_2') {
             var $left_options = Multiselect.$left.children(':visible');
             Multiselect.$right.eq(1).append($left_options);
 
@@ -60,28 +65,29 @@ $(document).ready(function() {
     function ownMoveToLeft(Multiselect, $options, event, silent, skipStack) {
         var button = $(event.currentTarget).attr('id');
 
-        if (button == 'multiselect1_leftSelected') {
+        var prefix = Multiselect.$left.attr("id");
+        if (button == prefix+'_leftSelected_1') {
             var $right_options = Multiselect.$right.eq(0).find('> option:selected');
             Multiselect.$left.append($right_options);
 
             if ( typeof Multiselect.callbacks.sort == 'function' && !silent ) {
                 Multiselect.$left.find('> option').sort(Multiselect.callbacks.sort).appendTo(Multiselect.$left);
             }
-        } else if (button == 'multiselect1_leftAll') {
+        } else if (button == prefix+'_leftAll_1') {
             var $right_options = Multiselect.$right.eq(0).children(':visible');
             Multiselect.$left.append($right_options);
 
             if ( typeof Multiselect.callbacks.sort == 'function' && !silent ) {
                 Multiselect.$left.find('> option').sort(Multiselect.callbacks.sort).appendTo(Multiselect.$left);
             }
-        } else if (button == 'multiselect2_leftSelected') {
+        } else if (button == prefix+'_leftSelected_2') {
             var $right_options = Multiselect.$right.eq(1).find('> option:selected');
             Multiselect.$left.append($right_options);
 
             if ( typeof Multiselect.callbacks.sort == 'function' && !silent ) {
                 Multiselect.$left.find('> option').sort(Multiselect.callbacks.sort).appendTo(Multiselect.$left);
             }
-        } else if (button == 'multiselect2_leftAll') {
+        } else if (button == prefix+'_leftAll_2') {
             var $right_options = Multiselect.$right.eq(1).children(':visible');
             Multiselect.$left.append($right_options);
 
